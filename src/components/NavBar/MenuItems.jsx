c4394c686755dd75ff22033dc487ab4c8bb02d5a
Gaurav V <gv71.msrit@gmail.com>
2021-05-03 15:23:30 +0530

    Updated navbar and added chilroutes and menubars

--- src/components/NavBar/MenuItems.jsx
+++ src/components/NavBar/MenuItems.jsx
@@ -3,36 +3,77 @@ export const MenuItems = [
     title: "Home",
     url: "/",
     cName: "nav-links",
+    hasChildRoutes : false,
   },
   {
     title: "Activities",
-    url: "/activities",
+    url: "#",
     cName: "nav-links",
+    hasChildRoutes : true,
+    childRoutes : [
+      {
+        title: "Education Drives",
+        url: "/activities/educational-drives",
+        cName: "child-links",
+      },{
+        title: "Eco Projects",
+        url: "/activities/eco-projects",
+        cName: "child-links",
+      },{
+        title: "Plantation Drives",
+        url: "/activities/plantation-drives",
+        cName: "child-links",
+      },{
+        title: "Cleaning Drives",
+        url: "/activities/cleaning-drives",
+        cName: "child-links",
+      }
+    ]
   },
   {
     title: "Announcements",
     url: "/announcements",
     cName: "nav-links",
+    hasChildRoutes : false,
   },
   {
     title: "Education",
     url: "#",
     cName: "nav-links",
+    hasChildRoutes : true,
+    childRoutes: [
+      {
+        title: "MSProgram",
+        url: "/education/ms-program/nelson-institute-for-environmental-studies",
+        cName: "child-links",
+      },{
+        title: "Trainings",
+        url: "/education/trainings",
+        cName: "child-links",        
+      },{
+        title: "Internships",
+        url: "/education/internships",
+        cName: "child-links",        
+      }
+    ]
   },
   {
     title: "Resources",
     url: "#",
     cName: "nav-links",
+    hasChildRoutes : false,
   },
   {
     title: "Contact Us",
     url: "/contact-us",
     cName: "nav-links",
+    hasChildRoutes : false,
   },
   {
     title: "Join Us",
     url: "/join-us",
     cName: "nav-links btn",
+    hasChildRoutes : false,
   },
   
 ];
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,8 +7,15 @@ import {Link} from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 import { BarsIcon, TimesIcon } from "react-line-awesome";
 
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
+
+
 
 import logo from "../../assets/Logo/whitelogo.png";
+import { red } from "@material-ui/core/colors";
 
 export default function NavBar() {
   const [clicked, setClicked] = useState(false);
@@ -28,7 +35,52 @@ export default function NavBar() {
     window.addEventListener("scroll", changeBackground);
   }, []);
 
-  
+  const [anchorEl1, setAnchorEl1] = React.useState(null); // activities
+  const [anchorEl2, setAnchorEl2] = React.useState(null); // education
+
+  const handleClick1 = (event) => {
+    setAnchorEl1(event.currentTarget);
+  };
+
+  const handleClick2 = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+
+  const handleClose1 = () => {
+    setAnchorEl1(null);
+  };
+
+  const handleClose2 = () => {
+    setAnchorEl2(null);
+  };
+
+  const StyledMenu = withStyles({
+    paper: {
+      backgroundColor: '#0e1217',
+      padding: '0'
+    },
+  })((props) => (
+    <Menu
+      elevation={0}
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      {...props}
+    />
+  ));
+
+  const StyledMenuItem = withStyles((theme) => ({
+    root: {
+      backgroundColor: "#0e1217",padding:0,
+    },
+  }))(MenuItem);
+
   return (
     <div>
       <nav className="navbar">
@@ -45,12 +97,63 @@ export default function NavBar() {
           <div className="nav-menu"> 
             {MenuItems.map((item, index) => {
               return (
-                <Link className={item.cName}  to={item.url} key={index} >
-                    {item.title}
-                </Link>
-                
+                  <div>
+                  {
+                    item.hasChildRoutes 
+                  ?  
+                    <div key={index}>
+                      { item.title == "Activities"
+                        ? <div className={item.cName} aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick1}>
+                            {item.title}
+                          </div>
+                        : <div className={item.cName} aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick2}>
+                            {item.title}
+                          </div>
+                      }
+                      { item.title == "Activities" 
+                      ? <StyledMenu
+                          id="simple-menu"
+                          anchorEl={anchorEl1}                    
+                          keepMounted
+                          open={Boolean(anchorEl1)}
+                          onClose={handleClose1}
+                        >
+                          {item.childRoutes.map((childItem, childIndex) => {
+                            return(
+                              <StyledMenuItem key={childIndex} onClick={handleClose1}>
+                                <Link className={childItem.cName} to={childItem.url}>
+                                  {childItem.title}
+                                </Link>
+                              </StyledMenuItem>
+                            );
+                          })}
+                        </StyledMenu>
+                      : <StyledMenu
+                          id="simple-menu"
+                          anchorEl={anchorEl2}
+                          keepMounted
+                          open={Boolean(anchorEl2)}
+                          onClose={handleClose2}
+                        >
+                          {item.childRoutes.map((childItem, childIndex) => {
+                            return(
+                              <StyledMenuItem key={childIndex} onClick={handleClose2}>
+                                <Link className={childItem.cName} to={childItem.url}>
+                                  {childItem.title}
+                                </Link>
+                              </StyledMenuItem>
+                            );
+                          })}
+                        </StyledMenu>                
+                      }
+                    </div>                     
+                  : <Link  className={item.cName}  to={item.url} key={index} >
+                      {item.title}
+                    </Link> 
+                  }
+                  </div>                          
               );
-            })}
+            })}             
           </div>
           <div className={clicked ? "mobile-nav" : "hide" } >
             {MenuItems.map((item, index) => {
