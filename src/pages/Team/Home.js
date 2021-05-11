b44ea5f45f01fe9e3f15eaa99492b4ec9a760446
Gaurav V <gv71.msrit@gmail.com>
2021-05-11 11:52:22 +0530

    updated team photos for home page

--- src/pages/Team/Home.js
+++ src/pages/Team/Home.js
@@ -1,11 +1,19 @@
 import malali from "../../assets/Team/malali.jpg";
-import gaurav from "../../assets/Team/gaurav.jpg";
-import divya from "../../assets/Team/divya.jpg";
-import fatima from "../../assets/Team/fatima.jpg";
+import mahantesh from "../../assets/Team/mahantesh.jpg";
+import nagendra from "../../assets/Team/nagendra.jpg";
+import pruthvi from "../../assets/Team/pruthvi.jpg";
 
 export const Members = [
-  { name: "Malali Gowda", src: malali, position: "Founder" },
-  { name: "Gaurav V", src: gaurav, position: "Website des" },
-  { name: "Divya", src: divya, position: "Website " },
-  { name: "Fatima", src: fatima, position: "Content manager" },
+  {
+    name: "Prof. Malali Gowda",
+    src: malali,
+    position: "Founding President & Trustee",
+  },
+  {
+    name: "Mahantesh Veerapur",
+    src: mahantesh,
+    position: "Secretary & Trustee",
+  },
+  { name: "Dr. Pruthvi Chakravarthi", src: pruthvi, position: "Trustee" },
+  { name: "Nagendra M Chander", src: nagendra, position: "Trustee" },
 ];
