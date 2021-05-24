c01d053b4c9842e8cc7b30a99b093f9bd9bb6bd3
Gaurav V <gv71.msrit@gmail.com>
2021-05-24 16:51:12 +0530

    setup node server with express

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongo = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+//const items = require("./routes/api/items");
+
+// body parser middleware
+app.use(express.json());
+
+// cors middleware
+app.use(cors({ origin: true, credentials: true }));
+
+// DB config
+//const db = require("./config/keys").mongoUri;
+
+// connect to mongodb
+// mongo
+//   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch((err) => console.log(err));
+
+// use routes
+//app.use("/api/items", items); // links all routes to items.js file in routes/api
+
+app.get("/", (req, res) => {
+  res.json("Hi from server");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log("Server active on port : ", PORT));
