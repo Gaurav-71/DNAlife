407f8a59755bb0e959bfb8eb2d13a17daa2c5b8e
Gaurav V <gv71.msrit@gmail.com>
2021-05-24 07:10:44 +0530

    setup redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.scss";
 
+// redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import ScrollToTop from "./ScrollToTop";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -31,56 +35,58 @@ import Courses from "./pages/Education/Courses/Courses.js";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="routes">
-          <ScrollToTop>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/contact-us" component={ContactUs} />
-              <Route exact path="/about-us" component={AboutUs} />
-              <Route exact path="/team" component={Team} />
-              <Route exact path="/join-us" component={JoinUs} />
-              <Route
-                exact
-                path="/education/ms-program/nelson-institute-for-environmental-studies"
-                component={MS}
-              />
-              <Route exact path="/education/trainings" component={Training} />
-              <Route
-                exact
-                path="/education/internships"
-                component={Internship}
-              />
-              <Route
-                exact
-                path="/education/certificate-courses"
-                component={Courses}
-              />
-              <Route
-                exact
-                path="/activities/educational-tours"
-                component={Education}
-              />
-              <Route exact path="/activities/eco-projects" component={Eco} />
-              <Route
-                exact
-                path="/activities/plantation-drives"
-                component={Plantation}
-              />
-              <Route
-                exact
-                path="/activities/cleaning-movements"
-                component={Cleaning}
-              />
-              <Route exact path="/announcements" component={Announcements} />
-            </Switch>
-          </ScrollToTop>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="routes">
+            <ScrollToTop>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/contact-us" component={ContactUs} />
+                <Route exact path="/about-us" component={AboutUs} />
+                <Route exact path="/team" component={Team} />
+                <Route exact path="/join-us" component={JoinUs} />
+                <Route
+                  exact
+                  path="/education/ms-program/nelson-institute-for-environmental-studies"
+                  component={MS}
+                />
+                <Route exact path="/education/trainings" component={Training} />
+                <Route
+                  exact
+                  path="/education/internships"
+                  component={Internship}
+                />
+                <Route
+                  exact
+                  path="/education/certificate-courses"
+                  component={Courses}
+                />
+                <Route
+                  exact
+                  path="/activities/educational-tours"
+                  component={Education}
+                />
+                <Route exact path="/activities/eco-projects" component={Eco} />
+                <Route
+                  exact
+                  path="/activities/plantation-drives"
+                  component={Plantation}
+                />
+                <Route
+                  exact
+                  path="/activities/cleaning-movements"
+                  component={Cleaning}
+                />
+                <Route exact path="/announcements" component={Announcements} />
+              </Switch>
+            </ScrollToTop>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
